721ee65b8925ddf371321991d6fb698b19ebde66
brianzzs <brianzanoni@outlook.com>
2025-03-26 19:26:57 -0300

    adding flag for analytics in local dev

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,4 +1,5 @@
-export const GA_MEASUREMENT_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID; 
+export const GA_MEASUREMENT_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+const DISABLE_ANALYTICS = import.meta.env.VITE_DISABLE_ANALYTICS === 'true';
 
 declare global {
     interface Window {
@@ -7,11 +8,13 @@ declare global {
 }
 
 export const logPageView = (page_path: string) => {
+    if (DISABLE_ANALYTICS) return;
     window.gtag('event', 'page_view', {
         page_path,
     });
 };
 
 export const logEvent = (action: string, params: object = {}) => {
+    if (DISABLE_ANALYTICS) return;
     window.gtag('event', action, params);
 }; 
